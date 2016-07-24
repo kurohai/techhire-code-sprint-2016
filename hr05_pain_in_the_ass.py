1a1af35234b7b0151748941d5233d43ab8f372ca
kurohai <kurohai@gmail.com>
2016-07-24 12:16:40 -0500

    first version, half working, maybe time issues

---
+++ hr05_pain_in_the_ass.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+def parse_next_string():
+    return raw_input().strip()
+
+
+def parse_next_int():
+    return int(parse_next_string())
+
+
+def parse_query_set():
+    return [parse_next_string() for i in xrange(parse_next_int())]
+
+
+def do_the_other_thing(query_set):
+    stop = len(max(query_set, key=len))
+    for i in xrange(stop):
+        new = [c for c in query_set if len(c) > i]
+        pre = [c[:i+1] for c in new]
+        if len(pre) == len(set(pre)):
+            return i
+
+
+def main():
+    set_count = parse_next_int()
+    for c in xrange(set_count):
+        query_set = parse_query_set()
+        print do_the_other_thing(query_set)
+
+
+if __name__ == '__main__':
+    main()
